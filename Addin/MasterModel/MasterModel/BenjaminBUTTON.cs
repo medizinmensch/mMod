f6d5ae566f811c138d842db7cdbeca712b327e7e
fxhy <hein.fa3@gmail.com>
2017-05-28 16:22:12 +0200

    versuch button zu erstellen

--- Addin/MasterModel/MasterModel/BenjaminBUTTON.cs
+++ Addin/MasterModel/MasterModel/BenjaminBUTTON.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inventor;
+using System.Drawing;
 
 namespace InvAddIn
 {
-    class BenjaminBUTTON
+    internal class BenjaminBUTTON : Button
     {
+        #region "Methods"
+
+        public BenjaminBUTTON(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, Icon standardIcon, Icon largeIcon, ButtonDisplayEnum buttonDisplayType)
+			: base(displayName, internalName, commandType, clientId, description, tooltip, standardIcon, largeIcon, buttonDisplayType)
+		{
+
+        }
+        public BenjaminBUTTON(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, ButtonDisplayEnum buttonDisplayType)
+			: base(displayName, internalName, commandType, clientId, description, tooltip, buttonDisplayType)
+		{
+
+        }
+
+        override protected void ButtonDefinition_OnExecute(NameValueMap context)
+        {
+            try
+            {
+                Sherlock sher = new Sherlock();
+                sher.Investigate();
+                sher.ShowShakespeare();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        #endregion
     }
 }
--- Addin/MasterModel/MasterModel/Shakespeare.cs
+++ Addin/MasterModel/MasterModel/Shakespeare.cs
@@ -10,7 +10,7 @@ namespace InvAddIn
     {
         public Shakespeare(MasterM MM)
         {
-            //test
+            
         }
     }
 }
