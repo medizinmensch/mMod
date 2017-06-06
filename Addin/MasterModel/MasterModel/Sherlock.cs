07edabff74533687a63c8f0ebf7bd49239070335
fxhy <hein.fa3@gmail.com>
2017-06-06 14:43:47 +0200

    2D Zeichnungen auslesen mit Test

--- Addin/MasterModel/MasterModel/Sherlock.cs
+++ Addin/MasterModel/MasterModel/Sherlock.cs
@@ -14,14 +14,31 @@ namespace InvAddIn
         MasterM partypart = new MasterM();
         public void Investigate(PartDocument suspect)
         {
+            List<string> test = new List<string>();
             //sketches
             foreach (Sketch sketchy in suspect.ComponentDefinition.Sketches)
             {
                 partypart.SketchyList.Add(sketchy);
-                MessageBox.Show(sketchy.AttributeSets.ToString());
+                //Test
+                test.Add("    "+sketchy.Name+": ");
+                foreach (Inventor.SketchEntity Ente in sketchy.SketchEntities)
+                {
+                    test.Add( Ente.Type.ToString());
+
+                }
+                //End Test
             }
+            //Test
+            var message = string.Join(",", test);
+            MessageBox.Show(message);
+            //End Test
+
             //Parameter
             partypart.param = suspect.ComponentDefinition.Parameters;
+            //Test
+            var message2 = string.Join(",", partypart.param.ToString());
+            MessageBox.Show(message2);
+            //End Test
         }
         public void ShowShakespeare()
         {
