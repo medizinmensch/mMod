4ea99c1d8fed7d7b4752df72149c55f2c05dcba8
Oskar <relias.rakso@gmail.com>
2017-07-10 19:20:51 +0200

    switching to old Keyhole.js

--- Web-App/src/scripts/jscad_scripts/Keyhole.js
+++ Web-App/src/scripts/jscad_scripts/Keyhole.js
@@ -1,13 +1,31 @@
 function main(params) {
 
 	//Sketch1: 
-	var polygon1 = CAG.fromPoints ( [[-24.12,0], [-24.12,2.828], [-17.588,2.828], [-11.133,9.438], [-6,19.782], [-1.467,0], [-24.12,0] ] );
-	
-	var sketch1 = polygon1;
-	var Revolution1 = rotate_extrude(sketch1);
+	var circle1 = CAG.circle ({ center:[params.circle1_CenterX, params.circle1_CenterY], radius: params.circle1_Radius});
+	var polygon2 = CAG.fromPoints ( [[3,-8.517], [3,-3.517], [-3,-3.517], [-3,-8.517], [3,-8.517] ] );
+	var sketch1 = union(circle1, polygon2);
+	var Extrusion1 = sketch1.extrude({ offset: [0,0,params.Extrusion1] });
 
-	var OskarTheGreat = Revolution1;
+	//Sketch2: 
+	var circle3 = CAG.circle ({ center:[params.circle3_CenterX, params.circle3_CenterY], radius: params.circle3_Radius});
+	var sketch2 = circle3;
+	var Extrusion2 = sketch2.extrude({ offset: [0,0,params.Extrusion2] });
+	var Subtraction1 = Extrusion1.subtract(Extrusion2);
+
+	var OskarTheGreat = union(Subtraction1);
 
 	return OskarTheGreat;
 }
 
+function getParameterDefinitions() {
+	return [
+		{ name: 'circle1_Radius', caption: 'radius of circle1', type: 'float', initial: '5', step: 0.1 },
+		{ name: 'circle1_CenterX', caption: 'X-Coordinate of circle1', type: 'float', initial: '0', step: 0.1 },
+		{ name: 'circle1_CenterY', caption: 'Y-Coordinate of circle1', type: 'float', initial: '-12.5', step: 0.1 },
+		{ name: 'Extrusion1', caption: 'Length of Extrusion1', type: 'float', initial: '10', step: 0.1 },
+		{ name: 'circle3_Radius', caption: 'radius of circle3', type: 'float', initial: '1.5', step: 0.1 },
+		{ name: 'circle3_CenterX', caption: 'X-Coordinate of circle3', type: 'float', initial: '0', step: 0.1 },
+		{ name: 'circle3_CenterY', caption: 'Y-Coordinate of circle3', type: 'float', initial: '-12.5', step: 0.1 },
+		{ name: 'Extrusion2', caption: 'Length of Extrusion2', type: 'float', initial: '10', step: 0.1 },
+	];
+}
